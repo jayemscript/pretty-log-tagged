656ea731800852a007fc4b7ee92c2ed55a8a27c0
jayemscript <johnmarkpulmano052802@gmail.com>
2026-04-28 16:19:23 +0800

    feat: fix browser issue logging to use bundling for browser

---
+++ src/browser.ts
@@ -0,0 +1,4 @@
+export { clog, createLogger } from "./logger";
+export type { PrettyLogger, LogFn, LoggerOptions } from "./logger";
+export type { TagConfig, BuiltinTag } from "./lib/tags";
+export { DEFAULT_TAGS } from "./lib/tags";
